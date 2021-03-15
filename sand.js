f533ce32c5044dafa8b01e489adc0ccad7a24b05
DevilGaming <77532381+Dhanigaming@users.noreply.github.com>
2021-03-15 14:36:41 +0530

    Add files via upload

--- sand.js
+++ sand.js
@@ -1,1 +1,30 @@
-
+class Sand {
+    constructor(x, y, width, height) {
+      var options = {
+        isStatic:false,
+        
+          'restitution':0.8,
+          'friction':0.3,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, 5, 5, options);
+      this.width = 5;
+      this.height = 5;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      strokeWeight(15);
+      stroke("voilet");
+      fill("blue");
+      rect(500,500, this.width, this.height);
+      pop();
+    }
+  };
+  
