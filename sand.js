e3ca05bda8e7da154caf215da017c964daf84dbe
DevilGaming <77532381+Dhanigaming@users.noreply.github.com>
2021-03-16 19:04:28 +0530

    Add files via upload

--- sand.js
+++ sand.js
@@ -7,11 +7,10 @@ class Sand {
           'friction':0.3,
           'density':1.0
       }
-        this.x=x;
-        this.y=y;
-        this.r=r 
-        this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
-        
+      this.body = Bodies.circle(x, y, (r-20)/2, options);
+      this.width = 5;
+      this.height = 5;
+      
       World.add(world, this.body);
     }
     display(){
